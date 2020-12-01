c6c90ae7efadc9a11034c19e25069cfb9989ed5a
evg <evgbv@hotmail.com>
2020-12-01 14:39:16 +0700

    fix test

--- src/services/getWorkSheduleService_test.go
+++ src/services/getWorkSheduleService_test.go
@@ -1,15 +1,27 @@
 package services
 
 import (
-	"testing"
 	"employee/src/repo"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetWorkSheduleSuccess(t *testing.T) {
-	service := services.NewWorkShedulerService(repo.NewEmployeeSheduleRepositury())
+	service := NewWorkShedulerService(repo.NewEmployeeSheduleRepositury())
 
 	result := service.GetShedule(1, "2020-01-01", "2020-02-01")
 
-	assert.Equal(t, result, services.SheduleResponse{Shedule: {&services.SheduleOnDay{Day: "2020-01-01", TimeRanges: {{"start":1000, "end":1300}, {"start":1000, "end":1300}}}}})
+	expectedResult := &SheduleResponse{
+		Shedule: []*SheduleOnDay{
+			{
+				Day: "2020-01-01",
+				TimeRanges: []map[string]int{
+					{"start": 1000, "end": 1300}, {"start": 1000, "end": 1300},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, result, expectedResult)
 }
